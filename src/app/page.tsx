4086b8d9aebd865351c265ac6312b5443ef9aed7
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-06-12 22:45:41 +0300

    created rotues of auth0 service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
-import Dashboard from "./pages/Dashboard/page";
+"use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Link from "next/link";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
   return (
     <div>
-      {/* <div className="">
+      <div className="">
         <h1>AI Prompt Generator</h1>
-        <button className="border-2 p-2 font-bold">Log In</button>
-      </div> */}
+        {!user ? (
+          <Link href="/api/auth/login">
+            <button className="border-2 p-2 font-bold">Login</button>
+          </Link>
+        ) : (
+          <>
+            <h1>Welcome, {user.name}</h1>
+            <Link href="/api/auth/logout">
+              <button>Logout</button>
+            </Link>
+          </>
+        )}
+      </div>
     </div>
   );
 }
