11e594931b0e00157dbf1cd04c301a54a796ce65
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-09-08 21:44:11 +0300

    taskSlice has been edited

--- store/features/tasks/taskSlice.ts
+++ store/features/tasks/taskSlice.ts
@@ -1,21 +1,46 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+interface Task {
+  id: number;
+  text: string;
+}
+
 interface taskState {
-  value: string;
+  tasks: Task[];
 }
 
 const initialState: taskState = {
-  value: "",
+  tasks: [],
 };
 
 const taskSlice = createSlice({
   name: "task",
   initialState,
   reducers: {
-    setTask: (state, action: PayloadAction<string>) => {
-      state.value = action.payload;
+    addTask: (state, action: PayloadAction<string>) => {
+      const newTask: Task = {
+        id: Date.now(),
+        text: action.payload,
+      };
+      state.tasks.push(newTask);
+    },
+    removeTask: (state, action: PayloadAction<number>) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    },
+    updateTask: (
+      state,
+      action: PayloadAction<{ id: number; newText: string }>
+    ) => {
+      const task = state.tasks.find((t) => t.id === action.payload.id);
+      if (task) {
+        task.text = action.payload.newText;
+      }
+    },
+    clearTasks: (state) => {
+      state.tasks = [];
     },
   },
 });
 
-export const { setTask } = taskSlice.actions;
+export const { addTask, removeTask, updateTask, clearTasks } =
+  taskSlice.actions;
