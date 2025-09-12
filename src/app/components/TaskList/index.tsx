b691030a513767c4b1bf5892fcad5b49072c90bd
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-09-12 23:37:58 +0300

    TaskList has been edited

--- src/app/components/TaskList/index.tsx
+++ src/app/components/TaskList/index.tsx
@@ -1,7 +1,33 @@
+"use client";
 import React from "react";
+import { useAppSelector, useAppDispatch } from "../../../../store/hooks";
+import { removeTask } from "../../../../store/features/tasks/taskSlice";
 
 const TaskList = () => {
-  return <div>TaskList</div>;
+  const tasks = useAppSelector((state) => state.task.tasks);
+  const dispatch = useAppDispatch();
+  const handleRemove = (id: number) => {
+    dispatch(removeTask(id));
+  };
+  return (
+    <div className="bg-white shadow-md rounded-lg p-4 max-w-md mx-auto space-y-4">
+      <h1 className="text-xl font-semibold text-center">Task List</h1>
+      {tasks.length === 0 && (
+        <p className="text-gray-500 text-center">No Task Yet..</p>
+      )}
+      <ul className="space-y-2">
+        {tasks.map((task) => (
+          <li
+            key={task.id}
+            className="flex justify-between items-center border-b pb-1"
+          >
+            <span>{task.text}</span>
+            <button onClick={() => handleRemove(task.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TaskList;
